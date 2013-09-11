49b828abfd1d202f082ba4d4b64363e1d4d7bf9b
Akshay Khole <akshay@reversehack.in>
2013-09-11 15:28:05 +0530

    Using Haml instead of Erb for views. Helps keeping the code clean and readable.

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby '2.0.0'
 
 gem 'sinatra'
 gem "unicorn"
+gem "haml", "~> 4.0.3"
 
 group :development do
   gem "shotgun"
--- app.rb
+++ app.rb
@@ -2,5 +2,5 @@ require 'sinatra'
 
 get '/' do
   @days = (Date.parse("2013/9/14").mjd - Date.today.mjd).to_i
-  erb :index
+  haml :index
 end
